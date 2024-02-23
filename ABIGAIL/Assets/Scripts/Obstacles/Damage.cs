d16e1c3d7320b8365b48248489fc0b549fc3903f
ethangoldberg13 <148988789+ethangoldberg13@users.noreply.github.com>
2024-02-23 15:57:39 -0500

    colliders test

--- ABIGAIL/Assets/Scripts/Obstacles/Damage.cs
+++ ABIGAIL/Assets/Scripts/Obstacles/Damage.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class Damage : MonoBehaviour
 {
     public int damageAmount = 10;
-     private void OnTriggerEnter(Collider other)
+     private void OnTriggerEnter2D(Collider2D other)
     {
+        Debug.Log("Triggered");
+        if (other.CompareTag("Abigail")){
+
+        
         // Check if the collider that entered is a player or an object that can take damage
         Health health = other.GetComponent<Health>();
 
@@ -15,6 +19,7 @@ public class Damage : MonoBehaviour
         {
             health.TakeDamage(damageAmount);
             Debug.Log("damage");
+        }
         }
     }
 }
