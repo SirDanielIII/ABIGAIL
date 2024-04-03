7650a3f32efe115e18f22f1edc6a1460e8faed9d
Joel Mills <joelarimills@gmail.com>
2024-04-03 12:07:17 -0400

    TopDown Abigail
    - top down abigail animations and assets done

--- ABIGAIL/Assets/Scripts/Abigail/Movement_Top.cs
+++ ABIGAIL/Assets/Scripts/Abigail/Movement_Top.cs
@@ -8,6 +8,14 @@ public class Movement_Top : MonoBehaviour
     public float speed = 22.0f;
     public Rigidbody2D rb;
     Vector2 movement; 
+    private Animator animator;
+    public float rotationSpeed = 720f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
 
     void Update()
     {
@@ -17,7 +25,21 @@ public class Movement_Top : MonoBehaviour
 
     void FixedUpdate()
     {
+        bool isRunning = movement.magnitude > 0;
+        animator.SetBool("IsRunning", isRunning);
+        
+        if (isRunning)
+        {
+            UpdateDirection();
+        }
         Vector2 normalizedMovement = movement.normalized;
         rb.MovePosition(rb.position + normalizedMovement * (speed * Time.fixedDeltaTime));
+    }
+
+    void UpdateDirection()
+    {
+        float angle = Mathf.Atan2(movement.y, movement.x) * Mathf.Rad2Deg;
+        Quaternion targetRotation = Quaternion.Euler(new Vector3(0, 0, angle - 90));
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
     }
 }
